46f43091c8a656f1851219e8ab657356a4c75a30
Horlatech001 <oladimejigobir@gmail.com>
2023-07-28 21:34:54 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,177 @@
+(function ($) {
+    "use strict";
+    
+    // Dropdown on mouse hover
+    $(document).ready(function () {
+        function toggleNavbarMethod() {
+            if ($(window).width() > 992) {
+                $('.navbar .dropdown').on('mouseover', function () {
+                    $('.dropdown-toggle', this).trigger('click');
+                }).on('mouseout', function () {
+                    $('.dropdown-toggle', this).trigger('click').blur();
+                });
+            } else {
+                $('.navbar .dropdown').off('mouseover').off('mouseout');
+            }
+        }
+        toggleNavbarMethod();
+        $(window).resize(toggleNavbarMethod);
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Date and time picker
+    $('#date').datetimepicker({
+        format: 'L'
+    });
+    $('#time').datetimepicker({
+        format: 'LT'
+    });
+
+
+    // Service carousel
+    $(".service-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        margin: 30,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+
+
+    // Team carousel
+    $(".team-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        margin: 30,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:2
+            },
+            768:{
+                items:3
+            },
+            992:{
+                items:4
+            }
+        }
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        center: true,
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 30,
+        dots: true,
+        loop: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+
+    // Counter
+
+  function startCounterAnimation() {
+    // Select all elements with the class "counter"
+    $('.counter').each(function () {
+      var $this = $(this);
+      var target = parseInt($this.attr('data-target')); // Get the target value
+
+      $({ countNum: $this.text() }).animate(
+        {
+          countNum: target,
+        },
+        {
+          duration: 2000, // Adjust the duration (in milliseconds) as per your preference
+          easing: 'linear',
+          step: function () {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function () {
+            $this.text(target + "+");
+          },
+        }
+      );
+    });
+  }
+  // Call the counter animation when the section is in the viewport
+  $(window).on('scroll', function () {
+    var offset = $('#counter-section').offset().top;
+    var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+    if (bottom_of_window > offset) {
+      startCounterAnimation();
+    }
+  });
+
+  // Smooth scrolling for navbar links
+  $(document).ready(function () {
+    $('a[href^="#"]').on('click', function (event) {
+      event.preventDefault();
+      var target = this.hash;
+      $('html, body').animate(
+        {
+          scrollTop: $(target).offset().top,
+        },
+        1000 // Adjust the scrolling speed as per your preference (in milliseconds)
+      );
+    });
+  });
+    
+})(jQuery);
+
